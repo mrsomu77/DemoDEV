322a6496c2608860723ef8464725be48173a08b4
AdmiN <AdmiN@ITWS065>
2024-08-29 10:00:08 +0530

    First commit

---
+++ DemoDev561/src/com/lbrce/demo561/Games.java
@@ -0,0 +1,10 @@
+package com.lbrce.demo561;
+
+public class Games {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("My game is Football");
+	}
+
+}
